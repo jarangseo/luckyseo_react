acab2c0d054bc64de939bff5eb729ae95b86d4c7
jarangseo <rangs919@gmail.com>
2025-12-07 07:59:25 +0900

    CategoryItem  & Directory component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
-  return (
-    <div className="categories-containers">
-      <div className="categories-container">{/* <img/> */}</div>
-    </div>
-  );
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      subtitle: "Shop now",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      subtitle: "Shop now",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      subtitle: "Shop now",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      subtitle: "Shop now",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      subtitle: "Shop now",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+
+  return <Directory categories={categories} />;
 };
 
 export default App;
