ec7a9f64623b8655235e2cb1a48be902bfef3a6c
jarangseo <rangs919@gmail.com>
2026-01-22 19:57:10 +0900

    Add product-card component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,14 @@
 import "./shop.styles.scss";
 import { useProducts } from "../../contexts/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
 
 const Shop = () => {
   const { products } = useProducts();
   console.log(products);
   return (
-    <div>
-      {products?.map((category) => (
-        <div key={category.id}>
-          <h1>{category.name}</h1>
-          <div>
-            {category.items?.map((item) => (
-              <div key={item.id}>{item.name}</div>
-            ))}
-          </div>
-        </div>
+    <div className="products-container">
+      {products?.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
