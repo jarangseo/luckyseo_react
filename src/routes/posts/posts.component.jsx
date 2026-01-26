1c197a9ad2c6740001943d65f64d90578c92c3ba
jarangseo <rangs919@gmail.com>
2026-01-26 16:50:53 +0900

    use prefetchQuery

--- src/routes/posts/posts.component.jsx
+++ src/routes/posts/posts.component.jsx
@@ -1,18 +1,29 @@
 import "./posts.styles.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { fetchPosts } from "../../api";
 import { queryKeys } from "../../utils/queryKeys";
 
+const totalPages = 10;
+
 const Post = () => {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const { data, isLoading, error, isError } = useQuery({
     queryKey: queryKeys.posts.list(currentPage),
-    queryFn: () => fetchPosts(currentPage + 1),
+    queryFn: () => fetchPosts(currentPage),
     // staleTime: 1000 * 60 * 5,
   });
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    queryClient.prefetchQuery({
+      queryKey: queryKeys.posts.list(currentPage + 1),
+      queryFn: () => fetchPosts(currentPage + 1),
+    });
+  }, [currentPage, queryClient]);
 
   // replace with useQuery
   // const data = [];
@@ -32,7 +43,14 @@ const Post = () => {
     return <div>No data available</div>;
   }
 
-  // console.log(data);
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
   return (
     <div className="post-container">
       <ul>
@@ -43,11 +61,11 @@ const Post = () => {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage === 1} onClick={handlePreviousPage}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= totalPages} onClick={handleNextPage}>
           Next page
         </button>
       </div>
