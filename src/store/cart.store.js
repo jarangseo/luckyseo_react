411041ffb1e74587cbc452139017b230897d1c04
jarangseo <rangs919@gmail.com>
2026-01-27 11:40:33 +0900

    Add devtools for Zustand

--- src/store/cart.store.js
+++ src/store/cart.store.js
@@ -1,89 +1,96 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 const useCartStore = create(
-  persist(
-    (set, get) => ({
-      // ============ State ============
-      isCartOpen: false,
-      cartItems: [],
+  devtools(
+    persist(
+      (set, get) => ({
+        // ============ State ============
+        isCartOpen: false,
+        cartItems: [],
 
-      // ============ Getters ============
-      getCartCount: () =>
-        get().cartItems.reduce((total, item) => total + item.quantity, 0),
+        // ============ Getters ============
+        getCartCount: () =>
+          get().cartItems.reduce((total, item) => total + item.quantity, 0),
 
-      getCartTotal: () =>
-        get().cartItems.reduce(
-          (total, item) => total + item.quantity * item.price,
-          0
-        ),
+        getCartTotal: () =>
+          get().cartItems.reduce(
+            (total, item) => total + item.quantity * item.price,
+            0
+          ),
 
-      // ============ Actions ============
-      actions: {
-        toggleCart: () => set((state) => ({ isCartOpen: !state.isCartOpen })),
+        // ============ Actions ============
+        actions: {
+          toggleCart: () =>
+            set((state) => ({ isCartOpen: !state.isCartOpen })),
 
-        setCartOpen: (isOpen) => set({ isCartOpen: isOpen }),
+          setCartOpen: (isOpen) => set({ isCartOpen: isOpen }),
 
-        addItemToCart: (productToAdd) =>
-          set((state) => {
-            const existingCartItem = state.cartItems.find(
-              (cartItem) => cartItem.id === productToAdd.id
-            );
+          addItemToCart: (productToAdd) =>
+            set((state) => {
+              const existingCartItem = state.cartItems.find(
+                (cartItem) => cartItem.id === productToAdd.id
+              );
 
-            if (existingCartItem) {
+              if (existingCartItem) {
+                return {
+                  cartItems: state.cartItems.map((cartItem) =>
+                    cartItem.id === productToAdd.id
+                      ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                      : cartItem
+                  ),
+                };
+              }
+
+              return {
+                cartItems: [
+                  ...state.cartItems,
+                  { ...productToAdd, quantity: 1 },
+                ],
+              };
+            }),
+
+          removeItemFromCart: (productToRemove) =>
+            set((state) => {
+              const existingCartItem = state.cartItems.find(
+                (cartItem) => cartItem.id === productToRemove.id
+              );
+
+              if (existingCartItem.quantity === 1) {
+                return {
+                  cartItems: state.cartItems.filter(
+                    (cartItem) => cartItem.id !== productToRemove.id
+                  ),
+                };
+              }
+
               return {
                 cartItems: state.cartItems.map((cartItem) =>
-                  cartItem.id === productToAdd.id
-                    ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                  cartItem.id === productToRemove.id
+                    ? { ...cartItem, quantity: cartItem.quantity - 1 }
                     : cartItem
                 ),
               };
-            }
+            }),
 
-            return {
-              cartItems: [...state.cartItems, { ...productToAdd, quantity: 1 }],
-            };
-          }),
-
-        removeItemFromCart: (productToRemove) =>
-          set((state) => {
-            const existingCartItem = state.cartItems.find(
-              (cartItem) => cartItem.id === productToRemove.id
-            );
-
-            if (existingCartItem.quantity === 1) {
-              return {
-                cartItems: state.cartItems.filter(
-                  (cartItem) => cartItem.id !== productToRemove.id
-                ),
-              };
-            }
-
-            return {
-              cartItems: state.cartItems.map((cartItem) =>
-                cartItem.id === productToRemove.id
-                  ? { ...cartItem, quantity: cartItem.quantity - 1 }
-                  : cartItem
+          clearItemFromCart: (cartItemToClear) =>
+            set((state) => ({
+              cartItems: state.cartItems.filter(
+                (cartItem) => cartItem.id !== cartItemToClear.id
               ),
-            };
-          }),
+            })),
 
-        clearItemFromCart: (cartItemToClear) =>
-          set((state) => ({
-            cartItems: state.cartItems.filter(
-              (cartItem) => cartItem.id !== cartItemToClear.id
-            ),
-          })),
-
-        clearCart: () => set({ cartItems: [] }),
-      },
-    }),
-    {
-      name: "cart-storage",
-      partialize: (state) => ({
-        cartItems: state.cartItems,
+          clearCart: () => set({ cartItems: [] }),
+        },
       }),
-    }
+      {
+        name: "cart-storage",
+        partialize: (state) => ({
+          cartItems: state.cartItems,
+        }),
+      }
+    ),
+    { name: "CartStore" }
   )
 );
 
